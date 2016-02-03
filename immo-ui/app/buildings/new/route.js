11662cb8cc384695002e1a53099ce648c7d7fcf2
Dooley <nsewolo@gmail.com>
2016-02-03 07:50:56 -0500

    opportunity cheking WIP

--- immo-ui/app/buildings/new/route.js
+++ immo-ui/app/buildings/new/route.js
@@ -2,7 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model(){
-    this.modelFor('building');
+  model: function(){
+    return {};
+  },
+
+  actions: {
+
+    create: function(model){
+
+      let building = this.store.createRecord('building', {
+        name: model.name,
+        type: model.type,
+        sellingPrice: model.sellingPrice,
+        numberOfFlats: model.numberOfFlats,
+        propertyTax: model.propertyTax,
+        schoolTax: model.schoolTax,
+        annualGrossRent: model.annualGrossRent,
+      });
+
+      building.save().then(() => {
+        this.transitionTo('buildings');
+        }, function() {
+          console.log('Building save failed');
+      });
+    }
   }
 });
