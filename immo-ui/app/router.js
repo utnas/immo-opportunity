685683507991e7a87090342a819370a7dc51f681
Dooley <nsewolo@gmail.com>
2016-02-12 23:07:53 -0500

    show route added

--- immo-ui/app/router.js
+++ immo-ui/app/router.js
@@ -7,12 +7,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('buildings', function() {
-    this.route('buildings', {path: 'index'});
     this.route('new');
-    this.route('show', {path: ':building_id'}, function(){
-      // Incase of nested routes
-    });
     //Other routes here ...
+    this.route('show', {path: '/:building_id'});
   });
 });
 
